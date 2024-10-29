4c87b3bea8943bfa5714ec2a50cb932569f5e160
Dhruvrudani00 <dhruvrudani00@gmail.com>
2024-10-29 09:02:19 +0000

    datatypes

---
+++ 01_basic/02_datatypes.js
@@ -0,0 +1,11 @@
+"use strict";
+// alert("Hello") // we are using nodeJs not browser
+
+
+let name = "Dhruv"
+let age = 21
+let isLoggedIn = true
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
+

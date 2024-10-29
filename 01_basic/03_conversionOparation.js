417888b480920d4aa37c2bb1cae7fea8ae898d83
Dhruvrudani00 <dhruvrudani00@gmail.com>
2024-10-29 09:23:11 +0000

    conversionOperation

---
+++ 01_basic/03_conversionOparation.js
@@ -0,0 +1,27 @@
+// let score = false
+
+// console.log(typeof score);
+
+// let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+// "33" => number
+// "33abc" => null
+// "" => 0
+// true => 1; false => 0
+
+let isLoggedIn = "Dhruv";
+
+let booleanIsLoggedIn = Boolean (isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => flase
+// "Dhruv" => true
+
+let somenumber = 33
+
+let stringNumber = String(somenumber)
+console.log(stringNumber);
+console.log( typeof stringNumber);

53aa77b8b8c4030ad5adcfb77aad4804091e7b4a
Dhruvrudani00 <dhruvrudani00@gmail.com>
2024-10-29 09:02:04 +0000

    variable

---
+++ 01_basic/01_variable.js
@@ -0,0 +1,11 @@
+const accountId = 144553;
+let accountEmail = "dhruv2103@gmail.com"
+var accountPassword = "12345"
+accountCity = "Surat"
+// accountId = 2 not allowed
+accountEmail = "Sahil2108@gmail.com"
+accountPassword = "232323"
+accountCity = "Ahemedabad"
+let accountState
+console.log (accountId)
+console.table ([accountId, accountEmail, accountPassword, accountCity, accountState])

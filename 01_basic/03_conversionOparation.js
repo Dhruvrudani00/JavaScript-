52189545196a20447049f4dc1b6ab42484ba0206
Dhruvrudani00 <dhruvrudani00@gmail.com>
2024-10-29 10:18:49 +0000

    operation

--- 01_basic/03_conversionOparation.js
+++ 01_basic/03_conversionOparation.js
@@ -14,7 +14,7 @@
 let isLoggedIn = "Dhruv";
 
 let booleanIsLoggedIn = Boolean (isLoggedIn);
-console.log(booleanIsLoggedIn);
+// console.log(booleanIsLoggedIn);
 
 // 1 => true; 0 => false
 // "" => flase
@@ -23,5 +23,26 @@ console.log(booleanIsLoggedIn);
 let somenumber = 33
 
 let stringNumber = String(somenumber)
-console.log(stringNumber);
-console.log( typeof stringNumber);
+// console.log(stringNumber);
+// console.log( typeof stringNumber);
+
+
+// **********************Operation**********************
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+
+let str1 = "Hello"
+let str2 = " Dhruv"
+
+let str3 = str1+ str2
+// console.log(str3);
+
+
+console.log("1" + 2); // 12
+console.log(1 + "2"); // 12
+console.log("1" + 2 + 2); // 122
+console.log(1 + 2 + "2"); // 32
+

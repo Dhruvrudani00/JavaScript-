59ec471c9948e39d8545babc45353988c12df061
Dhruvrudani00 <dhruvrudani00@gmail.com>
2024-10-30 12:08:54 +0000

    number_and_math

---
+++ 01_basic/06_nums_and_math.js
@@ -0,0 +1,23 @@
+const score = 400 // its directly define that score is number 
+
+// console.log (score) 
+
+const balance = new Number (100) // its specifically define that belance is number 
+
+// console.log (balance)
+
+// console.l    og (balance.toString().length)
+// console.log (balance.toFixed(2))
+
+const otherNumber = 123.8945
+
+// console.log (otherNumber.toPrecision(4))
+
+const hundreds = 1000000
+// console.log (hundreds.toLocaleString('en-IN'))
+
+
+// ******************************* Math *******************************
+
+console.log (Math)    
+console.log (Math.abs(-4))    

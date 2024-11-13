facf908d3f8ad6e3c995727695e50fdc5cbf44fe
Dhruvrudani00 <dhruvrudani00@gmail.com>
2024-11-13 12:31:04 +0000

    arrar2

---
+++ 02_basic/02_array.js
@@ -0,0 +1,10 @@
+const marvel_heros = ['Thor', 'Black Panther', 'IronMan']
+const dc_heros = ['Superman', 'Flash', 'Batman']
+
+// marvel_heros.push(dc_heros) // it can add array inside array
+
+// console.log(marvel_heros);
+
+const allHeros = marvel_heros.concat(dc_heros)
+console.log(allHeros);
+

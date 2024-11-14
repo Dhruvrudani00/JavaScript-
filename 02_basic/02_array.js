87f6733b78f0fe2b8caedcf6ca39aaf456f84a7c
Dhruvrudani00 <dhruvrudani00@gmail.com>
2024-11-14 04:28:54 +0000

    arraypart2

--- 02_basic/02_array.js
+++ 02_basic/02_array.js
@@ -5,6 +5,23 @@ const dc_heros = ['Superman', 'Flash', 'Batman']
 
 // console.log(marvel_heros);
 
-const allHeros = marvel_heros.concat(dc_heros)
-console.log(allHeros);
+// const allHeros = marvel_heros.concat(dc_heros)
+// console.log(allHeros);
 
+const allNewHeros = [...marvel_heros, ...dc_heros] // it can combine two array into one array.
+// console.log(allNewHeros);
+
+const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+
+const real_another_array = another_array.flat(Infinity) // it can combine array inside array and convert into one array.
+console.log(real_another_array);
+
+console.log(Array.isArray("Dhruv"));
+console.log(Array.from("Dhruv")); // it can convert string to the array.
+console.log(Array.from({name: 'Dhruv'})); // interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of(score1,score2,score3));

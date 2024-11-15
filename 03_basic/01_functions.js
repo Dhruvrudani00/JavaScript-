f1d37eea13721b9cef33c2901354590875c18c02
Dhruvrudani00 <dhruvrudani00@gmail.com>
2024-11-15 05:23:03 +0000

    function_object_array

--- 03_basic/01_functions.js
+++ 03_basic/01_functions.js
@@ -36,4 +36,32 @@ function loginUserMessage(username){
 }
 
 // console.log (loginUserMessage('Dhruv'))
-console.log (loginUserMessage())
+// console.log (loginUserMessage())
+
+function calculateCartPrice(val1, val2, ...num1){
+    return num1
+}
+
+// console.log(calculateCartPrice(200, 300, 500, 2000));
+
+const user = {
+    username: ' Dhurv ',
+    price : 199
+}
+
+function handleObject(anyobject){
+    console.log (`Username is ${anyobject.username} and price is ${anyobject.price}`);
+}
+// handleObject(user)
+handleObject({
+    username:'Sam',
+    price:399
+})  
+
+const myNewArr = [200,400,500,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+}
+// console.log(returnSecondValue(myNewArr));
+console.log(returnSecondValue([200,500,1000]));
